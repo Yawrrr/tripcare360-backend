e46de9dba0be3345f0352073261c069ae9c5972e
Yap Jun Cheng <cheng03@graduate.utm.my>
2026-06-13 15:34:27 +0800

    chore(infra): add migration to fix ProcessedAt column type to datetimeoffset

---
+++ Tripcare360.Infrastructure/Migrations/20260528084906_FixProcessedAtType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tripcare360.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProcessedAtType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ProcessedAt",
+                table: "Claims",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ProcessedAt",
+                table: "Claims",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+        }
+    }
+}
